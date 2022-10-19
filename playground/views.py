4445f34d76bac5da35610cc53cce26e31d352144
ISPritchin <ISPritchin@gmail.com>
2022-10-20 02:08:29 +0300

    add models

---
+++ playground/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+
+def say_hello(request: HttpRequest) -> HttpResponse:
+    return render(request, 'hello.html', context={
+        "name": "Ivan"
+    })
