8406d64e09ac9714dd47dc8196b6496e687e4e6e
ISPritchin <ISPritchin@gmail.com>
2022-10-20 15:46:52 +0300

    add comments

--- store/models.py
+++ store/models.py
@@ -2,12 +2,23 @@ from django.db import models
 
 
 class Collection(models.Model):
+    """
+    Набор / коллекция:
+    title - название коллекции
+    """
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Promotion(models.Model):
+    """
+    Акция:
+    description - описание акции
+    start_date - дата начала акции
+    end_date - дата окончания акции
+    discount - скидка
+    """
     description = models.CharField(max_length=255)
     start_date = models.DateField(auto_now=True)
     end_date = models.DateField(auto_now=True)
@@ -15,6 +26,17 @@ class Promotion(models.Model):
 
 
 class Product(models.Model):
+    """
+    Продукт:
+    title - наименование продукта
+    slug
+    description - описание продукта
+    unit_price - стоимость за единицу товара
+    inventory - инвентарный номер
+    last_update - последнее обновление записи
+    collection - id коллекции, к которой относится товар
+    promotions - id акции, к которой относится товар
+    """
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     description = models.TextField()
@@ -27,6 +49,15 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    """
+    Покупатель
+    first_name - имя
+    last_name - фамилия
+    email - адрес почты
+    phone - номер телефона
+    birth_date - дата дня рождения
+    membership - уровень членства
+    """
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
@@ -46,6 +77,12 @@ class Customer(models.Model):
 
 
 class Order(models.Model):
+    """
+    Заказ
+    placed_at - дата оформления
+    payment_status - статус заказа
+    customer - покупатель
+    """
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
     PAYMENT_STATUS_FAILED = 'F'
@@ -63,6 +100,13 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
+    """
+    Элемент заказа
+    order - заказ
+    product - товар
+    quantity - количество
+    unit_price - стоимость элемента заказа
+    """
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.SmallIntegerField()
@@ -70,17 +114,34 @@ class OrderItem(models.Model):
 
 
 class Address(models.Model):
+    """
+    Адрес
+    customer - покупатель
+    street - улица
+    city - город
+    zip - почтовый индекс
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     zip = models.CharField(max_length=20, null=True)
 
 
 class Cart(models.Model):
+    """
+    Корзина
+    created_at - дата создания
+    """
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
+    """
+    CartItem - элемент корзины
+    cart - корзина
+    product - продукт
+    quantity - количество
+    """
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
