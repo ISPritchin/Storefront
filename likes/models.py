63c628683259a69695d4333c3c9825b66e3e5d5f
ISPritchin <ISPritchin@gmail.com>
2022-10-21 16:58:53 +0300

    delete comment

--- likes/models.py
+++ likes/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.auth.models import User
 
 
 class LikedItem(models.Model):
