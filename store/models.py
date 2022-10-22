ce7afeb5174b907460dd4e865ce608103243791d
ISPritchin <ISPritchin@gmail.com>
2022-10-22 18:37:46 +0300

    update models

--- store/models.py
+++ store/models.py
@@ -10,6 +10,12 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
 
 class Promotion(models.Model):
     """
@@ -38,7 +44,7 @@ class Product(models.Model):
     promotions - id акции, к которой относится товар
     """
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(null=True)
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
