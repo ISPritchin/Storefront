dc34ed5809c3ae1c69aded52b1d3f2592effa53d
ISPritchin <ISPritchin@gmail.com>
2022-10-22 19:04:47 +0300

    change view and template

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.db.models import Q, F
 
+from store.models import Product, OrderItem
 
 def say_hello(request: HttpRequest) -> HttpResponse:
+    ids = OrderItem.objects.values('product_id').distinct()
+    query_set = Product.objects.filter(id__in=ids)
+
     return render(request, 'hello.html', context={
-        "name": "Ivan"
+        "name": "Ivan",
+        "products": list(query_set)
     })
