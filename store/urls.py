d57fa8c0807afa44522bd02950298f51219d1ae6
ISPritchin <ISPritchin@gmail.com>
2022-10-25 18:15:08 +0300

    add collection filter

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,11 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
-router = DefaultRouter()
-router.register('products', views.ProductViewSet)
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet, basename='products-detail')
 router.register('collections', views.CollectionViewSet)
 
-urlpatterns = router.urls
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+urlpatterns = router.urls + products_router.urls
